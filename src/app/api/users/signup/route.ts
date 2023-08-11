e2c4205a770aeb39d57478174afbc44d612f4b2f
arapsiyo <arapsiyo@protonmail.com>
2023-08-11 14:25:17 -0600

    initial commit

---
+++ src/app/api/users/signup/route.ts
@@ -0,0 +1,41 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { prisma } from '@/app/lib/prisma';
+import bcryptjs from 'bcryptjs';
+
+export const POST = async (req: NextRequest) => {
+  try {
+    const reqBody = await req.json();
+    const { username, email, password } = reqBody;
+    console.log(reqBody);
+
+    // check if user exit
+    const user = await prisma.user.findFirst({
+      where: {
+        email: email,
+      },
+    });
+    if (user) {
+      return NextResponse.json(
+        { error: 'User already exists' },
+        { status: 400 }
+      );
+    }
+    // hash password
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
+    const savedUser = await prisma.user.create({
+      data: {
+        username: username,
+        email: email,
+        password: hashedPassword,
+      },
+    });
+    return NextResponse.json({
+      message: 'User created successfully',
+      success: true,
+      savedUser,
+    });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+};
